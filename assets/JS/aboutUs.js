88050d7d9398979dc5f6519deb76f769153197a4
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-07-28 16:23:42 +0200

    section About Us sans le carousel

---
+++ assets/JS/aboutUs.js
@@ -0,0 +1,16 @@
+// document.body.onload=function(){
+//     let nbr=5;
+//     let p=0;
+//     let container= document.querySelector(".aboutUs__kitchen");
+//     console.log(container);
+//     let right= document.querySelector("#array_r");
+//     let left= document.querySelector("#array_l");
+//     container.style.width=(90*nbr)+"vw";
+//     for (let i = 1; i < nbr; i++) {
+//         let div= document.createElement('div');
+//         div.className="carousel__image";
+//         div.style.backgroundImage="url('assets/img/carousel'+i+'.jpeg')";
+//         container.appendChild(div);
+
+//     }
+// }
